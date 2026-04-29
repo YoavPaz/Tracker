541dff344c55ec575ddfd7433d05e44e05019617
YoavPaz <150516622+YoavPaz@users.noreply.github.com>
2026-04-29 18:30:15 +0300

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,16 +1,19 @@
 from robot.tracker import tracker
 from robot.camera_controller import camera_controller
 from robot.PID import PID
+from robot.MPU6050 import MPU6050
+from machine import I2C, Pin
 import time
-#import reset_all_pins
 
 camera = camera_controller(tx=2, rx=3)
 pid = PID(0 # sp
-          ,0.5 # kp
-          ,0 # ki
-          ,0 # kd
+          ,1.5 # kp
+          ,0.5 # ki
+          ,0.5 # kd
           )
-trac = tracker([9, 8], camera, pid)
+gyro_i2c = I2C(1, scl=Pin(11), sda=Pin(10), freq=400000)
+gyro = MPU6050(gyro_i2c)
+trac = tracker([9, 8], camera, pid, gyro)
 
 trac.run_servo(0, 90)
 trac.run_servo(1, 90)
@@ -18,9 +21,5 @@ trac.run_servo(1, 90)
 time.sleep(1)
 
 while True:
-    #trac.run_servo(0, 60)
-    #time.sleep(1)
-    
-    #trac.run_servo(0, 170)
-    #time.sleep(1)
     trac.follow_line()
+
